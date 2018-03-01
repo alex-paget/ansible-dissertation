3af9304aeb06a78efa83d8e3c6e45e3f0da4db62
alex-paget <frankpaget95@gmail.com>
2018-03-01 11:44:26 +0000

    Added netmask validation to deploy.py

--- deploy.py
+++ deploy.py
@@ -41,6 +41,15 @@ def ip_client(answer):
         else:
             print("Please give the host address in this format: 255")
 
+def netmask(answer):
+    while True:
+        input = raw_input(answer)
+        print(input)
+        if re.match("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$", input):
+            return True
+        else:
+            print("Please give the netmask in this format: 255.255.255.0")
+
 minimal = yes_no('Deploy Minimal Configuration\n')
 if minimal == True:
 
