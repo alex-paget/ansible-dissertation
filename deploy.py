c9d8c6835db84132fb947567197bd9fe5999626a
alex-paget <frankpaget95@gmail.com>
2018-03-28 17:31:47 +0100

    Added validation methods to deploy.py

--- deploy.py
+++ deploy.py
@@ -70,8 +70,52 @@ def netmask(answer, default):
         else:
             print("Please give the netmask in this format: 255.255.255.0")
 
+def is_alphanumeric(answer, default):
+    # Validates the input is alphanumeric
+    while True:
+        input = raw_input(answer)
+        # if the input matches the format return it
+        if input.isalpha() == True:
+            return input
+        # if the input is blank
+        elif input == "":
+            return default
+        # for anything else reprompt the user
+        else:
+            print("Please only use alphanumeric characters[abc123]")
+
+def generic(answer, default):
+    # Checks to make sure the entry isn't blank
+    input = raw_input(answer)
+    if input == "":
+        return default
+    else:
+        return input
+
+def is_num(answer, default):
+    # Validates the input is a number
+    while True:
+        input = raw_input(answer)
+        # if the input matches the format return it
+        if input.isdigit() == True:
+            return input
+        # if the input is blank return the defualt
+        elif input == "":
+            return default
+        else:
+            print("Please enter a number")
+
 minimal = yes_no('Deploy Minimal Configuration\n')
 if minimal == True:
+    ipaddr_network = ip_network("Please enter headnode IP network[192.168.0.]: ", "192.168.0.")
+    ipaddr_client = ip_client("Please enter headnoe IP client address[1]: ", "1")
+    repo = generic("Please enter the OpenHPC repo to use[http://build.openhpc.community/OpenHPC:/1.3/CentOS_7/x86_64/ohpc-release-1.3-1.el7.x86_64.rpm]:", "http://build.openhpc.community/OpenHPC:/1.3/CentOS_7/x86_64/ohpc-release-1.3-1.el7.x86_64.rpm")
+    ntp = generic("Please enter the NTP to use[0.centos.pool.ntp.org]: ", "0.centos.pool.ntp.org")
+    compute_name = generic("Please enter the name for the compute nodes[compute]: ", "compute")
+    compute_no = is_num("Please enter the number of compute nodes[4]: ", "4")
+
+
+
 
 ip_network("Please enter the headnode netowrk IP address\n")
 
