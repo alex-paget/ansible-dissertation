3c4a0c307fc7fb4b39e77c5bf35c3003f231f81d
alex-paget <frankpaget95@gmail.com>
2018-04-11 14:45:55 +0100

    Changed where variables reside

--- deploy.py
+++ deploy.py
@@ -326,7 +326,7 @@ else:
     # Resource startup
 
 # Write to file
-user_defined = [ipaddr_network, ip_client, sms_name, repo, ntp, compute_name, compute_no, socket_no, core_no, threads_no, ib_network, ib_client, interal_interface, ip_netmask, chroot, home_mount, opt_mount, beegfs_repo, beegfs_ip, lustre_mount, lustre_ip, nagios_user, nagios_pass, bmc_network, c_bmc_client, c_bmc_user, c_bmc_password, c_provision, c_ipaddr_network, c_ipaddr_client, c_ipoib_network, c_ipoib_client]
+user_defined = [ipaddr_network, ip_client, sms_name, repo, ntp, compute_name, compute_no, socket_no, core_no, threads_no, interal_interface, ip_netmask, chroot, home_mount, opt_mount, beegfs_repo, beegfs_ip, lustre_mount, lustre_ip, nagios_user, nagios_pass, bmc_network, c_bmc_client, c_bmc_user, c_bmc_password, c_provision, c_ipaddr_network, c_ipaddr_client]
 for x in range(len(user_defined)):
     if user_defined[x] != "":
         print(user_defined[x])
@@ -334,10 +334,12 @@ for x in range(len(c_mac)):
     print(c_mac[x])
 if infiniband_support != "":
     print(infiniband_support)
+    print(ib_network, ib_client)
 if omnipath_base != "":
     print(omnipath_base)
 if c_infiniband_support != "":
     print(c_infiniband_support)
+    print(c_ipoib_network, c_ipoib_client)
 if mem_limit != "":
     print(mem_limit)
 if ssh != "":
